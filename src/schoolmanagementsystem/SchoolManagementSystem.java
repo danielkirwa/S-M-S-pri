76090512fa08042a3f37a2fc3b720f7b993abec9
danielkirwa <kirwadan03@gmail.com>
2020-09-03 18:29:25 +0300

    Initial commit

---
+++ src/schoolmanagementsystem/SchoolManagementSystem.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagementsystem;
+
+/**
+ *
+ * @author owner
+ */
+public class SchoolManagementSystem {
+
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Readme Readme = new Readme();
+
+    }
+
+    private static class Readme {
+
+        public  Readme() {
+            
+        System.out.println("*******************************************************************");
+        System.out.println("############ School Management System ##############################");
+        System.out.println("##### Requires database prefered one is xampp(Sql)                 #");
+        System.out.println("##### Should be able to create teachers accounts for authorization #");
+        System.out.println("##### Take in new student details/archive alumina                  #");
+        System.out.println("##### Acount for students exams and analysis                       #");
+        System.out.println("##### Have a library module / book assets                          #");
+        System.out.println("##### Schedule time table                                          #");
+        System.out.println("##### Produce reports                                              #");
+        System.out.println("############     8/31/2020  daniel        ##########################");
+        System.out.println("********************************************************************");
+        }
+    }
+    
+    
+    
+}
